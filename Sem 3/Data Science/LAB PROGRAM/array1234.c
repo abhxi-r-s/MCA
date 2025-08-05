592b60b80a967620f13636267af2c95b0e281c18
abhxi-r-s <abhishekunni8281@gmail.com>
2025-08-05 11:36:32 +0530

    4th exercise program updated

---
+++ Sem 3/Data Science/LAB PROGRAM/array1234.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i;
+    printf("enter the number of elments");
+    scanf("%d",&n);
+    printf("enter the elements");
+    for(  i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
